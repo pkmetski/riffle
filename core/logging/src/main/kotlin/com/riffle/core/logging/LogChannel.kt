9b7170470ff6be396f1fefe3e637557c0c660f17
pkmetski <p.kmetski@gmail.com>
2026-07-27 09:34:08 +0200

    fix(logging): rename RIFFLE_ASYNC → RIFFLE_ANNSYNC for clarity

--- core/logging/src/main/kotlin/com/riffle/core/logging/LogChannel.kt
+++ core/logging/src/main/kotlin/com/riffle/core/logging/LogChannel.kt
@@ -24,5 +24,5 @@ enum class LogChannel(val tag: String) {
     Playlists("RIFFLE_PL"),
     ProgressSync("RIFFLE_PS"),
     Oom("RIFFLE_OOM"),
-    AnnotationSync("RIFFLE_ASYNC"),
+    AnnotationSync("RIFFLE_ANNSYNC"),
 }
