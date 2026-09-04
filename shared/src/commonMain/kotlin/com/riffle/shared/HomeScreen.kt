e648153fc638b0ba6fbe401ade33df39ebe764ce
pkmetski <p.kmetski@gmail.com>
2026-09-04 04:03:45 +0300

    fix(ios): add missing Column import in HomeScreen.kt

--- shared/src/commonMain/kotlin/com/riffle/shared/HomeScreen.kt
+++ shared/src/commonMain/kotlin/com/riffle/shared/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.riffle.shared
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicText
